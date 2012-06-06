3f90b96b9c5038b31d21f6597066316ce569b645
Marc Abramowitz <marc@marc-abramowitz.com>
2012-06-06 16:06:07 -0700

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
@@ -7,5 +7,6 @@ ext_modules = [Extension("hello", ["hello.pyx"])]
 setup(
   name = 'Hello world app',
   cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  ext_modules = ext_modules,
+  install_requires = ['Cython', 'Pyrex'],
 )
