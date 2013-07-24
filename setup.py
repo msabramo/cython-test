493c3c4fd42e9660ceecf0cfac0948f2adaaf7cb
Marc Abramowitz <marc@marc-abramowitz.com>
2013-07-24 13:02:54 -0700

    setup.py: Automatically install Cython if it's not installed

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ except ImportError:
     print("Couldn't import setuptools. Falling back to distutils.")
     from distutils.core import setup
 
+#
+# Force `setup_requires` stuff like Cython to be installed before proceeding
+#
+from setuptools.dist import Distribution
+Distribution(dict(setup_requires='Cython'))
+
 try:
     from Cython.Distutils import build_ext
 except ImportError:
@@ -19,6 +25,6 @@ setup(
   name = 'Hello world app',
   cmdclass = {'build_ext': build_ext},
   ext_modules = ext_modules,
-  install_requires = ['Cython'],
+  setup_requires = ['Cython'],
   test_suite='test',
 )
